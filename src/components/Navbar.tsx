612eadf89dfdf7ac9d25d55f8836cbc0250ca3c6
Xander <xanderwaugh531@gmail.com>
2023-10-04 13:21:06 -0400

    With NAv Links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,21 @@
+import Link from "next/link";
+import { NAV_ITEMS } from "~/utils";
+
 const Navbar: React.FC = () => {
   return (
     <nav
       role="navigation"
-      className="relative flex h-16 items-center justify-between bg-zinc-400 font-mono text-black shadow-sm"
+      className="relative flex h-16 items-center justify-between bg-zinc-400 px-4 font-mono text-black shadow-sm"
     >
       <h1>Navbar</h1>
+
+      <ul className="flex flex-row gap-4">
+        {NAV_ITEMS.map((item) => (
+          <Link key={item.label} href={item.href}>
+            <li>{item.label}</li>
+          </Link>
+        ))}
+      </ul>
     </nav>
   );
 };
