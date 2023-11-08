aace74943bd768b59fd1d25d54b2b4bffca72bc3
TaiMeade <ttmeade@radford.edu>
2023-11-08 13:28:54 -0500

    Temporary comment out of bugs...
    Commented out FeaturedRecipes page to temporarily fix bugs...also removed numLikes field from prisma schema

--- src/components/home/FeaturedRecipes.tsx
+++ src/components/home/FeaturedRecipes.tsx
@@ -4,25 +4,31 @@ import RecipeCard from "../recipe/RecipeCard";
 const AMT_RECIPES = 6;
 
 const FeaturedRecipes: React.FC = async () => {
-  const recipes = await prisma.recipe.findMany({
-    take: AMT_RECIPES,
-    where: { image: { not: null } },
-    include: {
-      comments: true,
-      author: { select: { name: true } },
-    },
-    orderBy: { likedBy: { _count: "desc" } },
-  });
+//   const recipes = await prisma.recipe.groupBy({
+//     take: AMT_RECIPES,
+//     by: ['id'],
+//     _count: {
+//       likedBy: true,
+//     }
+//     where: { image: { not: null } },
+//     include: {
+//       comments: true,
+//       author: { select: { name: true } },
+//     },
+//     orderBy: { _count },
+  // });
+// };
 
   return (
-    <div className="container mx-auto my-8">
-      <h2 className="mb-4 text-3xl font-semibold">Featured Recipes</h2>
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 ">
-        {recipes.map((recipe) => (
-          <RecipeCard key={recipe.id} recipe={recipe} />
-        ))}
-      </div>
-    </div>
+    <div>Test</div>
+    // <div className="container mx-auto my-8">
+    //   <h2 className="mb-4 text-3xl font-semibold">Featured Recipes</h2>
+    //   <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 ">
+    //     {recipes.map((recipe) => (
+    //       <RecipeCard key={recipe.id} recipe={recipe} />
+    //     ))}
+    //   </div>
+    // </div>
   );
 };
 
