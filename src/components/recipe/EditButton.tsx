f9b3482692b6650b4fdca1592b596897c1065bb6
CullenDan <cdanilowicz@radford.edu>
2023-11-09 11:40:09 -0500

    Edit button fixed redirect
    Edit button correctly redirects to the edit recipe page

--- src/components/recipe/EditButton.tsx
+++ src/components/recipe/EditButton.tsx
@@ -1,16 +1,28 @@
+"use client";
 import { type Recipe } from "@prisma/client";
-import React from "react";
-import Link from "next/link";
+import React, { useState } from "react";
+import { redirect } from "next/navigation";
 
 interface EditProps {
   recipe: Recipe;
 }
 const EditButton = ({ recipe }: EditProps) => {
+  const [redirecter, setRedirecter] = useState(false);
+  if (redirecter) {
+    return redirect("/recipes/edit/");
+  }
+
   return (
     <div>
-      <Link href="recipes/edit/" className="btn-grey btn">
-        Edit
-      </Link>
+      <button
+        className="btn-red btn flex flex-row items-center justify-center gap-2 text-lg disabled:btn-disabled "
+        onClick={() => setRedirecter(true)}
+      >
+        {"  "}
+        Edit{"  "}
+      </button>
+      {/* if(redirecter)
+      {redirect("/recipe/edit")} */}
     </div>
   );
 };
