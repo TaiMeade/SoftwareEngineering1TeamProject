17dd64e3247c6a7b1c06c0792c5e66f0ad543f18
CullenDan <cdanilowicz@radford.edu>
2023-12-06 23:08:35 -0500

    Name & Bio language

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 import { prisma } from "~/server/db";
 import { getAuth } from "~/server/session";
 import { updateUserSchema } from "~/utils/schemas";
+import Filter from "bad-words";
 
 export async function PUT(req: Request) {
   const session = await getAuth();
@@ -23,12 +24,21 @@ export async function PUT(req: Request) {
   }
 
   const data = parsed.data;
+  const filter = new Filter();
+
+  const filteredData = {
+    bio: data.bio ? filter.clean(data.bio) : data.bio,
+    username: data.username ? filter.clean(data.username) : data.username,
+  };
 
   // TODO Only fields that are not undefined will be updated
   try {
     const user = await prisma.user.update({
       where: { id: session.user.id },
-      data,
+      data: {
+        ...data,
+        ...filteredData,
+      },
     });
 
     return NextResponse.json(user, { status: 200, statusText: "OK" });
